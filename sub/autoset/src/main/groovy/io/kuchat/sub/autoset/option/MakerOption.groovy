14e0644ef0f71f0edaa9f6570ce248ac00352260
Changhwa Oh <changhwaoh.co@gmail.com>
2014-01-29 17:32:55 +0900

    프로젝트생성기 Helper옵션작업

---
+++ sub/autoset/src/main/groovy/io/kuchat/sub/autoset/option/MakerOption.groovy
@@ -0,0 +1,29 @@
+package io.kuchat.sub.autoset.option
+
+class MakerOption implements Option{
+
+    @Override
+    def job(def options) {
+        return null  
+    }
+
+    @Override
+    def projectName(def option) {
+        return null  
+    }
+
+    @Override
+    def ide(def option) {
+        return null  
+    }
+
+    @Override
+    def projectType(def option) {
+        return null  
+    }
+
+    @Override
+    def noOptions() {
+        return null  
+    }
+}
