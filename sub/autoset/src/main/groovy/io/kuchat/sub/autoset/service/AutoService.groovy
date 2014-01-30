3a01b54dcba2278d7a8d61e0d0a95862d7800f87
Changhwa Oh <changhwaoh.co@gmail.com>
2014-01-31 03:46:05 +0900

    build.gradle 파일 생성

--- sub/autoset/src/main/groovy/io/kuchat/sub/autoset/service/AutoService.groovy
+++ sub/autoset/src/main/groovy/io/kuchat/sub/autoset/service/AutoService.groovy
@@ -1,16 +1,20 @@
 package io.kuchat.sub.autoset.service
 
 import groovy.util.logging.Slf4j
+import io.kuchat.sub.autoset.template.GradleTemplate
+import io.kuchat.sub.autoset.template.Template
 import io.kuchat.sub.autoset.vo.OptionVo
 
 @Slf4j
 class AutoService {
 
+    Template template
+
     private AutoService(){}
 
     AutoService(OptionVo optionVo){
         makeProjectFolder(optionVo)
-
+        makeBuildGradle(optionVo)
     }
 
     /**
@@ -22,5 +26,18 @@ class AutoService {
         file.mkdir()
         log.info("$optionVo.projectPath 에 프로젝트 폴더를 생성하였습니다.")
     }
+
+    /**
+     * build.gradle 파일을 생성함
+     * @param optionVo
+     */
+    def makeBuildGradle(OptionVo optionVo){
+        template = new GradleTemplate()
+        def buildContent = template.template(optionVo)
+        File file = new File(optionVo.projectPath+"/build.gradle")
+        file << buildContent
+        log.info("build.gradle 파일을 생성하였습니다")
+    }
+
 
 }
