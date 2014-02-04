dfedd43e2c057816eb06db6c8dc90c44b4143830
Changhwa Oh <changhwaoh.co@gmail.com>
2014-02-04 18:03:46 +0900

    기본 com.mysql.jdbc.Driver

--- sub/autoset/src/main/groovy/io/kuchat/sub/autoset/template/DbPropertyTemplate.groovy
+++ sub/autoset/src/main/groovy/io/kuchat/sub/autoset/template/DbPropertyTemplate.groovy
@@ -5,7 +5,7 @@ class DbPropertyTemplate implements Template{
     @Override
     def template(options) {
 '''
-jdbc.driverClass=core.log.jdbc.driver.MysqlDriver
+jdbc.driverClass=com.mysql.jdbc.Driver
 jdbc.url=jdbc:mysql://127.0.0.1:3306/kuchat?useUnicode=true&amp;characterEncoding=utf8
 jdbc.username=kuchat
 jdbc.password=@kuchat!
