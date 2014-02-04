d448d8c10289e20ac85d977719f7a8b49f22b044
Changhwa Oh <changhwaoh.co@gmail.com>
2014-02-04 15:13:38 +0900

    Spring 리소스파일 설정

---
+++ sub/autoset/src/main/groovy/io/kuchat/sub/autoset/template/ResourceTemplate.groovy
@@ -0,0 +1,32 @@
+package io.kuchat.sub.autoset.template
+
+class ResourceTemplate implements Template{
+
+    @Override
+    def template(options) {
+        def resources = this."$options.projectType"(options)
+        resources
+    }
+
+    def jpa(options){
+'''
+<?xml version="1.0" encoding="UTF-8"?>
+<persistence xmlns="http://java.sun.com/xml/ns/persistence" version="2.0">
+
+    <persistence-unit name="Kuchat">
+        <properties>
+            <property name="hibernate.connection.url" value="jdbc:mysql://127.0.0.1:3306/kuchat?useUnicode=true&amp;amp;characterEncoding=utf8"/>
+            <property name="hibernate.connection.driver_class" value="com.mysql.jdbc.Driver"/>
+            <property name="hibernate.connection.username" value="kuchat"/>
+            <property name="hibernate.connection.password" value="@kuchat!"/>
+            <property name="hibernate.archive.autodetection" value="class"/>
+            <property name="hibernate.show_sql" value="true"/>
+            <property name="hibernate.format_sql" value="true"/>
+            <property name="use_sql_comments" value="true" />
+            <property name="hbm2ddl.auto" value="update"/>
+        </properties>
+    </persistence-unit>
+</persistence>
+'''
+    }
+}
