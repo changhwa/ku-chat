2f536644d49232c934bdc71a7c450d6304d0dc61
CoreaNim <coreanim@gmail.com>
2014-02-16 17:18:37 +0900

    테스트 센드 뮤직 클레스 작성

---
+++ server-music/src/test/java/TestSendMusic.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: KWS
+ * Date: 2014. 2. 16.
+ * Time: 오후 5:17
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestSendMusic {
+}
