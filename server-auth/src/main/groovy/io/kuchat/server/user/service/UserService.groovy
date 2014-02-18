0a864c8a052b6e3ec47913b3fdc25d6bcf74fcdb
Changhwa Oh <changhwaoh.co@gmail.com>
2014-02-19 01:53:45 +0900

    유저 기분상태 변경

--- server-auth/src/main/groovy/io/kuchat/server/user/service/UserService.groovy
+++ server-auth/src/main/groovy/io/kuchat/server/user/service/UserService.groovy
@@ -2,8 +2,11 @@ package io.kuchat.server.user.service
 
 import io.kuchat.server.auth.domain.User
 import io.kuchat.server.auth.repository.UserRepository
+import io.kuchat.server.user.domain.UserStatus
+import io.kuchat.server.user.repository.UserStatusRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 @Component
 class UserService {
@@ -11,8 +14,23 @@ class UserService {
     @Autowired
     UserRepository userRepository
 
+    @Autowired
+    UserStatusRepository userStatusRepository
+
     def findUserInfo(String email){
         User user = userRepository.findOne(email)
+        user
+    }
+
+    @Transactional
+    def updateUserStatus(UserStatus status){
+        UserStatus userStatus = userStatusRepository.findOne(status.email)
+        if (userStatus != null){
+            userStatus.feel = status.feel
+        }
+        User user = new User()
+        user.userStatus = userStatus;
+        user.email = userStatus.email
         user
     }
 
